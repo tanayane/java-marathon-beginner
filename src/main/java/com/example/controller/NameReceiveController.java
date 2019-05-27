26cff83c27f882ab0f641e18ec213f84be2ddb65
ayane.tanaka <ayane.tanaka@rakus.co.jp>
2019-05-27 14:32:38 +0900

    1km　完成

---
+++ src/main/java/com/example/controller/NameReceiveController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/nameprint")
+public class NameReceiveController {
+
+	@RequestMapping("")
+	public String index() {
+		return "inputname";
+	}
+	
+	@RequestMapping("/receive")
+	public String receive(String name,Model model) {
+		model.addAttribute("name",name);
+		return "outputname";
+	}
+}
